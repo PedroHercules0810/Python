a31240d9148032cfdd81f2ba2ae1e707b28df757
PedroHercules0810 <pedrosouzagrangeiro@gmail.com>
2023-08-14 15:17:32 -0300

    estudos básicos

---
+++ basico/if.py
@@ -0,0 +1,9 @@
+idade = 16
+maioridade = 16
+
+if idade > maioridade:
+    print("Já pode ir preso")
+elif idade < maioridade:
+    print("Ainda não pode ser preso")
+else:
+    print("completou agora a idade")
---
+++ basico/lacos.py
@@ -0,0 +1,13 @@
+x = [10,1,21,2,13,1,1,3,13,13];
+
+for y in x:
+    print(y);
+
+print();
+
+y = 0
+
+while y < 100:
+    if y % 10 == 0:
+        print(y);
+    y+=1
